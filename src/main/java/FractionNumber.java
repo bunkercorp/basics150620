f9c52533f41f3842e384a9a9774d26a768a2d813
SerhiyBogachenko <55495768+SerhiyBogachenko@users.noreply.github.com>
2020-07-16 19:19:58 +0300

    Сделал сложение и сокращение дробей

--- src/main/java/FractionNumber.java
+++ src/main/java/FractionNumber.java
@@ -1,4 +1,15 @@
 public class FractionNumber {
+
+    public static void main(String[] args) {
+        FractionNumber firstFraction = new FractionNumber(2, 4);
+        FractionNumber secondFraction = new FractionNumber(2, 4);
+        FractionNumber result = firstFraction.add(secondFraction);
+        System.out.println(result.dividend + "/" + result.divider);
+
+
+    }
+
+
     int dividend, divider;  // делимое , делитель
 
     FractionNumber(int dividend, int divider) {
@@ -11,22 +22,22 @@ public class FractionNumber {
         int newDividend, newDivider;
         newDividend = (dividend * fractionToAdd.divider) + (fractionToAdd.dividend * divider);
         newDivider = fractionToAdd.divider * divider;
-        FractionNumber resultFraction = new FractionNumber(newDividend,newDivider);
+        FractionNumber resultFraction = new FractionNumber(newDividend, newDivider);
         resultFraction.reduce();
         return resultFraction;
 
 
     }
-    public void reduce (){
-        for (int i = 2; i < 1000; i++){
-            if (divider%i == 0 || dividend%i ==0){
+
+    public void reduce() {
+        for (int i = 2; i < 1000; i++) {
+            if (divider % i == 0 || dividend % i == 0) {
                 divider = divider / i;
                 dividend = dividend / i;
                 i--;
             }
         }
     }
-
 
 
 }
