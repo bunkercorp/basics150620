98c9cf70663a59928a3272321fd871db80f88c9d
Teiidae <oku.prebeta@gmail.com>
2020-07-19 19:53:57 +0200

    hm7 main

---
+++ src/main/java/homeworks/hw7/Main.java
@@ -0,0 +1,19 @@
+package homeworks.hw7;
+public  class Main {
+    public static void main(String[] argv){
+        FractionNumber fr= new FractionNumber(12,7);
+        FractionNumber fradd = new FractionNumber(1,2);
+        FractionNumber res;
+        System.out.println("fr = " + fr.toString());
+        System.out.println("fradd = " + fradd.toString());
+        res=fr.add(fradd);
+        System.out.println("add = " + res.toString());
+        res=fr.subtract(fradd);
+        System.out.println("substr = " + res.toString());
+        res=fr.multiply(fradd);
+        System.out.println("mult = " + res.toString());
+        res=fr.divide(fradd);
+        System.out.println("div = " + res.toString());
+    }
+
+}
