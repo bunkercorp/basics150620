56974fde4d91fc703e89ad08d2f9a47ad11fa8dc
Silentroze <silentrozez@gmail.com>
2020-07-22 21:02:59 +0300

    HW8

--- HW8/src/com/hillelhometasks/HWEight/Logger.java
+++ HW8/src/com/hillelhometasks/HWEight/Logger.java
@@ -4,12 +4,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public abstract class Logger {
+    protected static StdLogger log;
+
     Logger(){}
-    int counter = 1;
-    protected void log(String solo){
+    int tik = 1;
+    public void log(String solo){
         final String method = Thread.currentThread().getStackTrace()[2].getMethodName();
         String dateFunc = new SimpleDateFormat("HH:MM:ss:SS").format(new Date());
-        System.out.printf("%d %s [%s]: %s\n", ++counter,dateFunc,method,solo);
+        System.out.printf("%d %s [%s]: %s\n", ++tik,dateFunc,method,solo);
         System.out.println();
     }
 }
--- HW8/src/com/hillelhometasks/HWEight/StdLogger.java
+++ HW8/src/com/hillelhometasks/HWEight/StdLogger.java
@@ -1,13 +1,22 @@
 package com.hillelhometasks.HWEight;
 
 public class StdLogger extends Logger  {
-    Logger logger = new Logger() {
-        @Override
-        protected void log(String solo) {
-            super.log(solo);
-        }
-    };
+
+
+   private static StdLogger _instance;
+
+    private StdLogger(){}
+    public static StdLogger Instance (){
+       if (_instance ==null){
+
+           _instance = new StdLogger();
+       }
+       return _instance;
+   }
+
     public static void main(String[] args) {
+        for (int i = 0 ; i<10 ; i ++)
+        System.out.println(log);
 
     }
 }
