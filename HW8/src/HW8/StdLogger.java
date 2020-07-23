2c71694bdf0dbe6e4322c85d624cfc58cefc592c
Silentroze <silentrozez@gmail.com>
2020-07-23 09:37:49 +0300

    HW8

---
+++ HW8/src/HW8/StdLogger.java
@@ -0,0 +1,32 @@
+package HW8;
+
+
+public class StdLogger extends Logger {
+    private static StdLogger _instance;
+
+    private StdLogger() {
+    }
+
+    public static StdLogger get_instance() {
+        if (_instance == null)
+            _instance = new StdLogger();
+        return _instance;
+    }
+    @Override
+    public void log(String date) {
+        System.out.printf("%s%s\n", logPrefix(), date);
+    }
+
+    public static void main(String[] args) {
+        StdLogger stdLogger = new StdLogger();
+        for (int i = 0; i <10  ; i++) {
+            stdLogger.log("test");
+
+
+        }
+
+    }
+
+}
+
+
--- HW8/src/com/hillelhometasks/HWEight/Main.java
+++
@@ -1,7 +0,0 @@
-package com.hillelhometasks.HWEight;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
