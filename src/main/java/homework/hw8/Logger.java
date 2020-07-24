6505c74e88504230810754c26ba8434e2d4ca4c9
Tetyana425 <izabon.va@gmail.com>
2020-07-24 12:00:43 +0300

    HW8

---
+++ src/main/java/homework/hw8/Logger.java
@@ -0,0 +1,10 @@
+package main.java.homeworks.hw8;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class Logger {
+
+   public abstract String log(String s);
+
+}
---
+++ src/main/java/homework/hw8/StdLogger.java
@@ -0,0 +1,34 @@
+package main.java.homeworks.hw8;
+
+import java.util.*;
+import java.text.*;
+
+  final public class StdLogger extends Logger {
+     private static StdLogger instance ;
+      int number = 1;
+      private  StdLogger(){
+
+      }
+
+    public static StdLogger getInstance(){
+          if(instance == null) {
+            instance = new StdLogger();
+          }
+          return instance;
+      }
+    @Override
+     public String log(String s){
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss:SSS");
+        String date = timeFormat.format(new Date());
+        final String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+        String str = String.format("%d %s [%s]: %s", number++, date, methodName, s) ;
+        return str;
+    }
+
+      public static void main(String[] args) {
+        StdLogger  instance = getInstance();;
+        System.out.println(instance.log("TEST"));
+        System.out.println(instance.log("TEST2"));
+        System.out.println(instance.log("TEST3"));
+      }
+}
