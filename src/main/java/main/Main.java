5ad55a864e0b4d223cd6903061e2bf463aba7578
Skazke <Skazke91@gmail.com>
2020-10-01 19:51:20 +0200

    TEST

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("porivet");System.out.println("porivet");
+        System.out.println("porivet");
+        System.out.println("porivet");
+        System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");
+        System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");
+        System.out.println("porivet");System.out.println("porivet");
+        System.out.println("porivet");
+        System.out.println("porivet");
+        System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");
+        System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");System.out.println("porivet");
+
+    }
+}
