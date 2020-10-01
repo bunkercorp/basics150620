71fe91bb8b3bfeaab37834104fb81451cfa1b7ea
SerhiyBogachenko <55495768+SerhiyBogachenko@users.noreply.github.com>
2020-10-01 19:40:30 +0300

    empty Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,32 +11,33 @@ import java.net.URL;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws MalformedURLException, InterruptedException {
-        if (args.length > 0) {
-            System.out.println("Arguments");
-            for (String arg : args) System.out.println("\t" + arg);
-        }
-        final Map<Object, Object> jvmProps = System.getProperties();
-        if (!jvmProps.isEmpty()) {
-            System.out.println("JVM props");
-            jvmProps.forEach((key, value) -> System.out.printf("\t%s=%s\n", key, value));
-        }
-        final String browserName = System.getProperty("browser");
-        DesiredCapabilities caps;
-        if (browserName.contentEquals("chrome")) {
-            caps = DesiredCapabilities.chrome();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");
-            caps.setCapability(ChromeOptions.CAPABILITY, options);
-        } else {
-            caps = DesiredCapabilities.firefox();
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--start-maximized");
-            caps.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
-        }
-        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.2.69:4444/wd/hub"), caps);
-        driver.get("https://google.com.ua");
-        Thread.sleep(3000);
-        driver.quit();
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+//        if (args.length > 0) {
+//            System.out.println("Arguments");
+//            for (String arg : args) System.out.println("\t" + arg);
+//        }
+//        final Map<Object, Object> jvmProps = System.getProperties();
+//        if (!jvmProps.isEmpty()) {
+//            System.out.println("JVM props");
+//            jvmProps.forEach((key, value) -> System.out.printf("\t%s=%s\n", key, value));
+//        }
+//        final String browserName = System.getProperty("browser");
+//        DesiredCapabilities caps;
+//        if (browserName.contentEquals("chrome")) {
+//            caps = DesiredCapabilities.chrome();
+//            ChromeOptions options = new ChromeOptions();
+//            options.addArguments("--start-maximized");
+//            caps.setCapability(ChromeOptions.CAPABILITY, options);
+//        } else {
+//            caps = DesiredCapabilities.firefox();
+//            FirefoxOptions options = new FirefoxOptions();
+//            options.addArguments("--start-maximized");
+//            caps.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
+//        }
+//        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.2.69:4444/wd/hub"), caps);
+//        driver.get("https://google.com.ua");
+//        Thread.sleep(3000);
+//        driver.quit();
     }
 }
